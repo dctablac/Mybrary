34c3bc0c1b1873ca3380c8e335e00029c590ed5b
Dan Tablac <dantablac@Dans-MacBook-Pro.local>
2020-05-24 14:17:15 -0700

    Ability to add, search, and view authors

--- README.md
+++ README.md
@@ -1,18 +1,14 @@
 ## NOTES:
 To install dependencies:
 
-  -- npm init -y
-  
-    ** in package.json, change give "main" value "server.js"
-    
-  -- npm i express ejs express-ejs-layouts
-  
-    ** in package.json, delete test and add "devStart" : "nodemon server.js"
-    
-  -- npm i --save-dev nodemon
-  
-    ** adds nodemon as a dependency, live refresh of server when changes are made
+  -- npm install
     
 To run the server:
 
   -- npm run devStart (development)
+
+Commit to Heroku
+
+  -- heroku git:remote -a mybrary-dctablac
+
+  -- git push heroku master
--- server.js
+++ server.js
@@ -5,14 +5,17 @@ if (process.env.NODE_ENV !== 'production') {    // if in development,
 const express = require('express');  // import express
 const app = express();               // app portionß
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index.js');
+const authorRouter = require('./routes/authors.js');
 
 app.set('view engine', 'ejs');   // set view engine to ejs
 app.set('views', __dirname + '/views');   // views folder in curr dir
 app.set('layout', 'layouts/layout'); // helps for staple content shared amongst pages (like header and footer)
 app.use(expressLayouts);    // using express layouts
 app.use(express.static('public'));   // styles, imgs, js
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose');    // requires mongoose tool from npm
 mongoose.connect(process.env.DATABASE_URL, {  // connect to Mongoose
@@ -24,6 +27,7 @@ db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to Mongoose'));
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 app.listen(process.env.PORT || 3000);    // deployment: server tells us what port is used or defaults to 3000
 
